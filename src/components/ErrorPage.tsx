1df5e4f0e6c996298c56882d6954201804e5bfc2
Cirstea Cosmin <cosmiinn97@gmail.com>
2022-08-27 19:51:57 +0300

    first commit

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import {useNavigate} from 'react-router-dom'
 
 function ErrorPage() {
+  let navigate = useNavigate();
   return (
-    <div>ErrorPage</div>
+    <section>
+      <div className='container--error--page'>
+
+        <h1>THIS WEB PAGE DOES NOT EXIST!</h1>
+        <p className='emoji--error'>😐</p>
+        <button
+          className="main--container--button"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          Back to Homepage
+        </button>
+
+      </div>
+    </section>
   )
 }
 
